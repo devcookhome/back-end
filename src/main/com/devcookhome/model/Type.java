2600593edb71ddc0cb6e493dfc9de1817a9d9e67
Leonardo <leomsouza62@gmail.com>
2020-05-26 18:21:08 -0300

    Dominio Type adicionado

---
+++ src/main/com/devcookhome/model/Type.java
@@ -0,0 +1,13 @@
+package com.devcookhome.model;
+
+public class Type{
+
+	private String type;
+
+	public void setType(String type){
+		this.type = type;
+	}
+	public String getType(){
+		return this.type;
+	}
+}
