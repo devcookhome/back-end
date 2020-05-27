cba64a1bae92ecdedad1d9992a8c88d3388b8e03
Lucas <lucass.trevisann@gmail.com>
2020-05-27 17:20:54 -0300

    Dominio Portion adicionado

---
+++ src/main/com/devcookhome/model/Portion.java
@@ -0,0 +1,24 @@
+package com.devcookhome.model;
+
+public class Portion{
+
+	private Integer quantity;
+	private String description;
+
+	public void setQuantity(Integer quantity){
+		this.quantity = quantity;
+	}
+
+	public Integer getQuantity(){
+		return this.quantity;
+	}
+
+	public void setDescription(String description){
+		this.description = description;
+	}
+
+	public String getDescription(){
+		return this.description;
+	}
+
+ }
