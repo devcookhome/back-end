28808b730ea0529137d70383baa41a1ae32c1296
Leonardo <leomsouza62@gmail.com>
2020-05-28 18:36:28 -0300

    Domínio Ingredient corrigido os Deffects

--- src/main/com/devcookhome/model/Ingredient.java
+++ src/main/com/devcookhome/model/Ingredient.java
@@ -1,9 +1,12 @@
 package com.devcookhome.model;
 
+import java.util.List;
+
 public class Ingredient{
 
+	private List<Type> type;
+	
 	private String name;
-	private String type;
 
 	public void setName (String name){
 		this.name = name;
@@ -11,11 +14,11 @@ public class Ingredient{
 	public String getName(){
 		return this.name;
 	}
-
-	public void setType(String type){
+	public void setType(List<Type> type){
 		this.type = type;
 	}
-	public String getType(){
+
+	public List<Type> getType(){
 		return this.type;
 	}
 }	
