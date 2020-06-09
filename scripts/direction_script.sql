e29c2941867e6aedcb026a9fa48385c8e8619a57
Lucas <lucass.trevisann@gmail.com>
2020-06-08 22:00:33 -0300

    novas adiçoes

--- scripts/direction_script.sql
+++ scripts/direction_script.sql
@@ -3,7 +3,7 @@ USE dev_cookhome;
 drop table if exists direction;
 
 create table direction(
-	id INT AUTO_INCREMENT;
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	tempo INT(10),
 	timeunity VARCHAR(50),
 	description VARCHAR(50)
