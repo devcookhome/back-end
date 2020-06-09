3c5213d05eea966632604d65e1e5a41c52f964b0
Leonardo <leomsouza62@gmail.com>
2020-06-08 21:45:39 -0300

    Feita a correção de um código

--- scripts/direction_script.sql
+++ scripts/direction_script.sql
@@ -3,8 +3,8 @@ USE dev_cookhome;
 drop table if exists direction;
 
 create table direction(
-	id INT AUTO_INCREMENT;
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	tempo INT(10),
 	timeunity VARCHAR(50),
-	descricao VARCHAR(50)
+	description VARCHAR(50)
 )
