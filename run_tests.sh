03599a9151e5373db9c93b3a7225456c180f3080
Lucas <lucass.trevisann@gmail.com>
2020-06-11 16:00:08 -0300

    Conexão com o a Database OK, Testes OK

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sh build.sh
-java -ea -classpath "$(pwd)/src/main/java/:$(pwd)/src/test/java/" com.devcookhome.dao.ConnectionManagerTest
+java -ea -classpath "$(pwd)/lib/mysql-connector-java-8.0.20.jar:$(pwd)/src/main/java/:$(pwd)/src/test/java/" com.devcookhome.dao.ConnectionManagerTest
--- src/test/java/com/devcookhome/dao/ConnectionManagerTest.java
+++ src/test/java/com/devcookhome/dao/ConnectionManagerTest.java
@@ -1,17 +1,35 @@
 package com.devcookhome.dao;
+import java.sql.Connection;
 
 public class ConnectionManagerTest{
 
 	
 	public static void main(String[] args) {
 	
+		testGetInstance();
+		testGetConnection();
+
+	}
+
+	public static void testGetInstance(){
+
 		System.out.print("ConnectionManagerTest#getInstance");
 
 		ConnectionManager cm = ConnectionManager.getInstance();
 		assert cm != null;
 
 		System.out.println("... OK");
-		
+	}
 
+
+	public static void testGetConnection(){
+
+		System.out.print("ConnectionManagerTest#getConnection");
+
+		ConnectionManager cm = ConnectionManager.getInstance();
+		Connection connection = cm.getConnection();
+		assert connection != null;
+
+		System.out.println("... OK");
 	}
 }
