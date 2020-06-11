99ff4b7f98fcc99c2c3e73a10b104f0a58edd59f
Lucas <lucass.trevisann@gmail.com>
2020-06-11 15:09:24 -0300

    Build e estrutura de testes OK

--- src/test/java/com/devcookhome/dao/ConnectionManagerTest.java
+++ src/test/java/com/devcookhome/dao/ConnectionManagerTest.java
@@ -5,10 +5,12 @@ public class ConnectionManagerTest{
 	
 	public static void main(String[] args) {
 	
-		System.out.println("ConnectionManagerTest#getInstance");
+		System.out.print("ConnectionManagerTest#getInstance");
 
 		ConnectionManager cm = ConnectionManager.getInstance();
 		assert cm != null;
+
+		System.out.println("... OK");
 		
 
 	}
