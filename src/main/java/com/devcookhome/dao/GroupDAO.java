120ac0f9ea7f7656d0607fd947fa63a45625a032
Lucas Trevisan <lucass.trevisann@gmail.com>
2020-06-20 13:03:31 -0300

    List, Update and Delete add

--- src/main/java/com/devcookhome/dao/GroupDAO.java
+++ src/main/java/com/devcookhome/dao/GroupDAO.java
@@ -3,9 +3,11 @@ package com.devcookhome.dao;
 import com.devcookhome.model.Group;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupDAO{
     private static final String TABLE = "group1";
@@ -20,7 +22,7 @@ public class GroupDAO{
         //SQL Injections.
         //Essa abordagem trivial está sendo utilizado com efeito didático.
         String sql = "INSERT INTO " + TABLE + "(" + FIELD_NAME + ") VALUES ('"
-            + group.getName() + "');";
+        + group.getName() + "');";
         try{
             //Prepara o objeto que exectua os comandos no banco    
             Statement statement = connection.createStatement();
@@ -36,5 +38,97 @@ public class GroupDAO{
             ex.printStackTrace();
         }
         return group;
+    }   
+
+    public static List<Group> list(){
+
+        String sql = "SELECT * FROM " + TABLE;
+
+        List<Group> groups = new ArrayList<Group>();
+
+        PreparedStatement pstm = null;
+        ResultSet result = null;
+
+        try {
+
+            pstm = connection.prepareStatement(sql);
+
+            result = pstm.executeQuery();
+
+
+            while(result.next()){
+
+                Group group = new Group();
+                group.setId(result.getLong(FIELD_ID));
+                group.setName(result.getString(FIELD_NAME));
+                groups.add(group);
+
+            }
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }finally{
+
+            try{
+
+                if(result != null){
+                    result.close();
+                }
+
+                if(pstm != null){
+                    pstm.close();
+                }
+
+
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+
+            return groups;
+        } 
+    }
+
+    public static Boolean update(Group group){
+
+        String sql = "UPDATE " + TABLE + " SET " + FIELD_NAME + " = ? WHERE " + FIELD_ID + " = ?";
+
+        PreparedStatement pstm = null;
+        
+        try {
+
+            pstm = connection.prepareStatement(sql);
+
+            pstm.setString(1, group.getName());
+            pstm.setLong(2, group.getId());
+
+            pstm.execute();
+            pstm.close();
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public static Boolean delete(Group group){
+
+        String sql = "DELETE FROM " + TABLE + " WHERE " + FIELD_ID + " = ?";
+
+        PreparedStatement pstm = null;
+
+        try {
+
+            pstm = connection.prepareStatement(sql);
+
+            pstm.setLong(1, group.getId());
+
+            pstm.execute();
+            pstm.close();
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
     }
 }
--- src/test/java/com/devcookhome/dao/GroupDAOTest.java
+++ src/test/java/com/devcookhome/dao/GroupDAOTest.java
@@ -2,6 +2,7 @@ package com.devcookhome.dao;
 
 import com.devcookhome.model.Group;
 import java.sql.Connection;
+import java.util.List;
 
 public class GroupDAOTest{
 
@@ -9,6 +10,9 @@ public class GroupDAOTest{
 	public static void main(String[] args) {
 	
 		testSave();
+		testList();
+		testUpdate();
+		testDelete();
 
 	}
 
@@ -26,5 +30,49 @@ public class GroupDAOTest{
 		System.out.println("... OK");
 	}
 
+	public static void testList(){
 
+		System.out.print("GroupDAOTest#list");
+
+		List<Group> groups = GroupDAO.list();
+
+		if (groups.size() == 0){
+			throw new RuntimeException("Expected Groups size > 0 ");
+		}
+
+		System.out.println("... OK");
+	}
+
+	public static void testUpdate(){
+
+		System.out.print("GroupDAOTest#update");
+
+		List<Group> groups = GroupDAO.list();
+
+		Group g = groups.get(0);
+		g.setName("Carnes");
+
+		Boolean b = GroupDAO.update(g);
+
+		assert b;
+
+		System.out.println("... OK");
+	}
+
+	public static void testDelete(){
+
+		System.out.print("GroupDAOTet#delete");
+
+		List<Group> groups = GroupDAO.list();
+
+		Group g = groups.get(0);
+		Integer size = groups.size();
+
+		assert GroupDAO.delete(g);
+		assert size > GroupDAO.list().size();
+
+		System.out.println("... OK");
+
+
+	}
 }
