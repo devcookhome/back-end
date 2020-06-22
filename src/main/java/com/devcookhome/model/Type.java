b749ddcd8a36b1d748233b456bf84b40ed8e0ef9
Lucas Trevisan <lucass.trevisann@gmail.com>
2020-06-22 20:11:19 -0300

    New Stuff

--- src/main/java/com/devcookhome/model/Type.java
+++ src/main/java/com/devcookhome/model/Type.java
@@ -2,7 +2,15 @@ package com.devcookhome.model;
 
 public class Type{
 
+	private Long id;
 	private String type;
+
+	public void setId(Long id){
+		this.id = id;
+	}
+	public Long getId(){
+		return this.id;
+	}
 
 	public void setType(String type){
 		this.type = type;
