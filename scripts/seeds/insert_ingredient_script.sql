146e39bcf0259bc972c0daa9929262ee8d202302
Leonardo <leomsouza62@gmail.com>
2020-06-23 20:21:45 -0300

    Feito a correção no código de seed

---
+++ scripts/seeds/insert_ingredient_script.sql
@@ -0,0 +1,6 @@
+USE dev_cookhome;
+
+INSERT INTO ingredient (name, type_id) VALUES ('Ovo','3'),('Açúcar','7'),('Café', '11'),('Leite','9'),
+('Farinha de Trigo','8'),('Fermento em pó','6'),('Macarrão','10'), ('Carne Moída','3'),('Água','9'),('Sardinha','5'),
+('Banana','2'),('Mamão','2'),('Laranja','2'), ('Abacaxi','2'),('Maçã','2'),('Arroz','11'),('Sal','6'),('Alho','6'),('Alface','1'),
+('Tomate','2');
