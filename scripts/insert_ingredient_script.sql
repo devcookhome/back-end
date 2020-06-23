b65bb65b1d475c811a3948286875f9d4cecf77a9
Leonardo <leomsouza62@gmail.com>
2020-06-23 18:53:30 -0300

    Seeds ingredient added

---
+++ scripts/insert_ingredient_script.sql
@@ -0,0 +1,6 @@
+USE dev_cookhome;
+
+INSERT INTO ingredient (name) VALUES ('Ovo'),('Açúcar'),('Café'),('Leite'),
+('Farinha de Trigo'),('Fermento em pó'),('Macarrão'), ('Carne Moída'),('Água'),('Sardinha'),
+('Banana'),('Mamão'),('Laranja'), ('Abacaxi'),('Maçã'),('Arroz'),('Sal'),('Alho'),('Alface'),
+('Tomate');
