d617c4d11eaa5875c83c37af5d5823be89f727ae
Lucas Trevisan <lucass.trevisann@gmail.com>
2020-07-03 14:36:34 -0300

    Fix on RecipeIngredient // Recipe

--- scripts/recipe_ingredient_script.sql
+++ scripts/recipe_ingredient_script.sql
@@ -5,6 +5,8 @@ DROP TABLE IF EXISTS recipe_ingredient;
 CREATE TABLE recipe_ingredient(
 	
 	recipe_id INT(6) NOT NULL,
-	ingredient_id INT(6) NOT NULL
+	ingredient_id INT(6) NOT NULL,
+	qtd VARCHAR(50),
+
 		
 );
