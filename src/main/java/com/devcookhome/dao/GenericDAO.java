af4d97692711081e03a9e7418d81b66ec21643ac
Lucas Trevisan <lucass.trevisann@gmail.com>
2020-07-04 11:06:07 -0300

    GenericDAO iniciado

--- src/main/java/com/devcookhome/dao/GenericDAO.java
+++ src/main/java/com/devcookhome/dao/GenericDAO.java
@@ -1,5 +1,6 @@
 package com.devcookhome.dao;
 
+import com.devcookhome.model.Entity;
 import com.devcookhome.model.Group;
 
 import java.sql.Connection;
@@ -9,12 +10,37 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupDAO{
+
+public class GenericDAO{
     private static final String TABLE = "group1";
     private static final String FIELD_ID = "id";
     private static final String FIELD_NAME = "name";
     private static Connection connection = ConnectionManager.getInstance().getConnection();
 
+    public static Entity save(Entity entity){
+        if(entity == null) return entity;
+        //Ha outras maneiras mais elegante para concatenar strings. 
+        //Inclusive a melhor maneira é setar as variaveis em preparedStatement para evitar
+        //SQL Injections.
+        //Essa abordagem trivial está sendo utilizado com efeito didático.
+        String sql = "INSERT INTO " + entity.getTableName() + "(" + entity.getFieldName() + ") VALUES ('"
+        + entity.getFieldValue() + "');";
+        try{
+            //Prepara o objeto que exectua os comandos no banco    
+            Statement statement = connection.createStatement();
+            //Executa a instrução no banco
+            statement.executeUpdate(sql);
+            //ler último id inserido para inserir no objeto criado
+            ResultSet result = statement.executeQuery("SELECT LAST_INSERT_ID() AS lastId;");
+
+            while(result.next()) {
+                entity.setId(result.getLong("lastId"));
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return entity;
+    }   
 
     public static List<Group> list(){
 
