3c63e40ca35a594d600e89325d6447b92a6d7012
Lucas Trevisan <lucass.trevisann@gmail.com>
2020-07-10 17:51:32 -0300

    Mini Server Created and working

---
+++ src/main/java/com/devcookhome/web/HomeHandler.java
@@ -0,0 +1,22 @@
+package com.devcookhome.web;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.*;
+
+public class HomeHandler implements HttpHandler {
+	@Override
+	public void handle(HttpExchange t) throws IOException {
+		String response = getResponse();
+		t.sendResponseHeaders(200, response.length());
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+	}
+	private String getResponse(){
+ 
+		return "<!DOCTYPE html><html><head><title>CookHome</title><meta charset=\"utf-8\"></head><body><h1>Bem vindo ao CookHome</h1></body></html>";
+
+	}
+}
+
