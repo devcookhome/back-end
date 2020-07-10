eb5f830b2df292a79a01a6d85e660e2dd0495961
Lucas Trevisan <lucass.trevisann@gmail.com>
2020-07-10 18:22:41 -0300

    Handler for Groups Created and Working

--- src/main/java/com/devcookhome/web/App.java
+++ src/main/java/com/devcookhome/web/App.java
@@ -6,10 +6,13 @@ import java.net.InetSocketAddress;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        HomeHandler handler = new HomeHandler();
+        HomeHandler homeHandler = new HomeHandler();
+        GroupHandler groupHandler = new GroupHandler();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/home", handler);
-        server.createContext("/inicio", handler);
+        server.createContext("/home", homeHandler);
+        server.createContext("/inicio", homeHandler);
+        server.createContext("/grupos", groupHandler);
+        server.createContext("/groups", groupHandler);
         server.setExecutor(null);
         server.start();
     }
