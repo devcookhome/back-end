904ee88291683140422e6f0662816561c8831d0d
lucas <you@example.com>
2020-10-24 11:51:21 -0300

    WIP recipes

--- spring-version/cookhome/src/main/java/com/devcookhome/web/HomeController.java
+++ spring-version/cookhome/src/main/java/com/devcookhome/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.devcookhome.web;
 
+import com.devcookhome.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private RecipeService recipeService;
 
 	@GetMapping(value={"/home", "/inicio"})
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	public String home(Model model) {
+		
+		model.addAttribute("recipes", recipeService.findAll());
 		return "home";
 	}
 
@@ -19,5 +24,6 @@ public class HomeController {
 		model.addAttribute("name", name);
 		return "storage";
 	}
+
 
 }
